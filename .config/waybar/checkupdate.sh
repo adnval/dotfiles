d30904ecb4bea98c2eac9c66393c2afa6b85f2e0
Kevin <kevin6700@proton.me>
2024-06-17 14:15:29 -0400

    feat: refined waybar style

--- .config/waybar/checkupdate.sh
+++ .config/waybar/checkupdate.sh
@@ -4,11 +4,11 @@ UPDATES=`checkupdates | wc -l`
 
 re='^[0-9]+$'
 if ! [[ $UPDATES =~ $re ]] ; then
-   echo "Failed to check updates"; exit 0
+   echo "failed to check updates"; exit 0
 fi
 
 if (( $UPDATES > 0 ));then
   echo "${UPDATES} updates available"; exit 0
 else
-  echo "System is up to date"; exit 0
+  echo "system is up to date"; exit 0
 fi
